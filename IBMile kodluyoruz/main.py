f3cfad8463c439c894a8317bfef89c6dada9e816
berke <117515381+berkebeyazbenli@users.noreply.github.com>
2024-05-01 20:05:33 +0300

    Add files via upload

---
+++ IBMile kodluyoruz/main.py
@@ -0,0 +1,38 @@
+import math
+
+def main():
+    points = [(2,3),(2,3),(4,5),(6,7),(8,9),(11,23)]
+    
+    distances=[]
+    
+    for i in range(len(points)):
+        for j in range(i+1, len(points)):
+            x = eucladian_dis(points[i], points[j])
+            distances.append(x)
+    
+    
+    minEle=100
+    
+    for i in range(len(distances)):
+        if distances[i]<minEle:
+            minEle=distances[i]
+    
+    print("Minimum uzaklık: ", minEle )
+    
+    
+def eucladian_dis(point1, point2):
+    distance = math.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
+    
+    return distance
+
+if __name__ == "__main__":
+    main()
+            
+        
+        
+    
+
+
+
+
+    
